f6eb508793fda33b0d2e6c3455ee489a60089cd5
sachinbandgar <sachinbandgar10@gmail.com>
2022-12-09 23:54:38 +0530

    variable codes
    variable codes

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,10 @@
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before swap:",num1, num2, num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After Swap:",num1, num2, num3);
+
---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var myFullName; // Variable Declaration
+console.log(myFullName); //variable declaration without initilization
+myFullName="Sachin Bandgar";
+console.log(myFullName);
+myFullName="Sachin Dadaso Bandgar";//Update
+console.log(myFullName);
+var myLove="I love only JS";//Declare and initialization
+console.log(myLove);
+var firstName="Sachin";
+console.log(firstName);
+var lastName;
+lastName="Bandgar";
+console.log(lastName);
+var age=27;
+console.log(age);
+var address="Tal. Sangola Dist. Solapur";
+console.log(address);
+var pin_code=413307;
+console.log(pin_code);
+var gender="male";
+console.log(gender);
+var is_married=true;
+console.log(is_married);
+
