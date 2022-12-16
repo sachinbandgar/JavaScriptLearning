d7c919d7e50b02cde3442a6e65fd510b0198a81a
sachinbandgar <sachinbandgar10@gmail.com>
2022-12-16 17:38:40 +0530

    interesting facts and log result on console
    interesting facts and log result on console

---
+++ 04_1nanAss.js
@@ -0,0 +1,20 @@
+console.log(`======== Interesting Fact ============`);
+
+var num2 = (1==false);
+console.log(`1) one is equal to equal to false : `,num2);
+
+var num2 = (0==false);
+console.log(`2) one is equal to equal to false : `,num2);
+
+var num3 = (null == undefined);
+console.log(`3) null is equal to equal to undefined : `,num3);
+
+var num4 = (1==[1]);
+console.log(`4) one is equal to equal to in sq. bracket one : `,num4);
+
+var num5 = (1==true);
+console.log(`5) one is equal to equal to true : `,num5);
+
+var num6 = (1=='1');
+console.log(`6) one is equal to equal to string one : `,num6);
+
