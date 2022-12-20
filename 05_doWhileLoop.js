e3a703ad423d904098892999675cf495f0c027de
sachinbandgar <sachinbandgar10@gmail.com>
2022-12-20 23:11:42 +0530

    while Loop assignment
    while Loop assignment

---
+++ 05_doWhileLoop.js
@@ -0,0 +1,32 @@
+for (let index = 5; index <= 15; index++) {
+   console.log(index);
+    
+}
+
+for (let dec = 50; dec >=40 ; dec--) {
+ console.log(dec);
+    
+}
+for (let odd =1; odd <=30 ; odd=odd+2) {
+    console.log(odd);
+       
+   }
+   for (let incr =0; incr <=20 ; incr=incr+2) {
+    console.log(incr);
+       
+   }
+
+   for (let even =5; even <=50 ; even=even+5) {
+    console.log(even);
+       
+   }
+
+   for (let incr =10; incr <=100 ; incr=incr+10) {
+    console.log(incr);
+       
+   }
+
+   for (let reverse =100; reverse >=10 ; reverse=reverse-10) {
+    console.log(reverse);
+       
+   }
